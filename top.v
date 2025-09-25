75124471bb1bc62f7144a3ad6606d98b2cb10f98
Daylenmathews <151976934+Daylenmathews@users.noreply.github.com>
2025-09-25 15:13:25 -0600

    Update top.v

--- top.v
+++ top.v
@@ -1,1 +1,29 @@
-
+module top(
+    input [6:0]sw, 
+    output [1:0]led
+    );
+   
+  
+  wire circuit_a_out;
+  circuit_a circuit_a_inst(
+   .A(sw[0]),
+   .B(sw[1]),
+   .C(sw[2]),
+   .D(sw[3]),
+   .Y(circuit_a_out)
+   
+ );
+ 
+ 
+ circuit_b circuit_b_inst(
+    .A(circuit_a_out),
+    .B(sw[4]),
+    .C(sw[5]),
+    .D(sw[6]),
+    .Y(led[1])
+ 
+ );
+ assign led[0]=circuit_a_out;
+ endmodule
+   
+  
